bc9ca59d060f78abe3fb07a310a5999fce6c4e71
e0c9 <tanhaoyc@gmail.com>
2019-01-10 20:33:06 +0800

    still have a ton of bugs, relax for a while

--- kvraft/client.go
+++ kvraft/client.go
@@ -9,6 +9,7 @@ import (
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// modify this struct
+	leaderIndex int
 }
 
 func nrand() int64 {
@@ -21,7 +22,7 @@ func nrand() int64 {
 func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
-	// code here
+	ck.leaderIndex = 0
 	return ck
 }
 
@@ -29,13 +30,47 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 //
 // send RPC; ok := ck.server[i].Call("RaftKV.Get",&args, &reply)
 func (ck *Clerk) Get(key string) string {
-	// modify this function
-	return ""
+	DPrintf("send a Get request: key(%v)", key)
+	args := &GetArgs{}
+	args.Key = key
+	reply := &GetReply{}
+
+	idx := ck.leaderIndex
+	for {
+		if idx == len(ck.servers) {
+			idx = 0
+		}
+		ok := ck.servers[idx].Call("RaftKV.Get", args, reply)
+		if ok && !reply.WrongLeader {
+			if reply.Err == ErrNoKey {
+				return ""
+			}
+			return reply.Value
+		}
+		idx++
+	}
 }
 
 // shared by Put and Append
 func (ck *Clerk) PutAppend(key string, value string, op string) {
-	// modify this function
+	DPrintf("send a Put request: key(%v)->value(%v)", key, value)
+	args := &PutAppendArgs{}
+	args.Key = key
+	args.Value = value
+	args.Op = op
+	reply := &PutAppendReply{}
+
+	idx := ck.leaderIndex
+	for {
+		if idx == len(ck.servers) {
+			idx = 0
+		}
+		ok := ck.servers[idx].Call("RaftKV.PutAppend", args, reply)
+		if ok && !reply.WrongLeader {
+			return
+		}
+		idx++
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- kvraft/server.go
+++ kvraft/server.go
@@ -6,9 +6,10 @@ import (
 	"raft-go/labrpc"
 	"raft-go/raft"
 	"sync"
+	"time"
 )
 
-const Debug = 0
+const Debug = 1
 
 func DPrintf(format string, a ...interface{}) (n int, err error) {
 	if Debug > 0 {
@@ -19,6 +20,10 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 
 type Op struct {
 	// difinitions here
+	Type  string
+	Key   string
+	Value string
+	Index int
 }
 
 type RaftKV struct {
@@ -30,21 +35,102 @@ type RaftKV struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// definitions here
+	store    map[string]string
+	response map[int]chan Result
+}
+
+type Result struct {
+	value string
+	err   Err
 }
 
 func (kv *RaftKV) Get(args *GetArgs, reply *GetReply) {
-	// code here
+	DPrintf("server receve a Get request")
+	op := Op{}
+	op.Key = args.Key
+	op.Type = "Get"
+	index, _, isLeader := kv.rf.Start(op)
+	if !isLeader {
+		reply.WrongLeader = true
+		return
+	}
+	reply.WrongLeader = false
+	kv.mu.Lock()
+	kv.response[index] = make(chan Result, 1)
+	kv.mu.Unlock()
+	select {
+	case res := <-kv.response[index]:
+		reply.Err = res.err
+		reply.Value = res.value
+	case <-time.After(1 * time.Second):
+		reply.WrongLeader = true
+	}
 }
 
 func (kv *RaftKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
+	DPrintf("server receive PutAppend request")
+	op := Op{}
+	op.Key = args.Key
+	op.Value = args.Value
+	op.Type = args.Op
+	index, _, isLeader := kv.rf.Start(op)
+	if !isLeader {
+		reply.WrongLeader = true
+		return
+	}
+	DPrintf("BOOM BOOM BOOM, this server is a Leader.")
+	reply.WrongLeader = false
+	kv.response[index] = make(chan Result)
+	DPrintf("index 值为 %v", index)
+	select {
+	case res := <-kv.response[index]:
+		DPrintf("收到结果了 为什么出问题")
+		reply.Err = res.err
+	case <-time.After(10 * time.Second):
+		DPrintf("=======================================")
+		reply.WrongLeader = true
+	}
+}
+
+func (kv *RaftKV) Kill() {
+	kv.rf.Kill()
 	// code here
 }
 
-func (kv *RaftKV) kill() {
-	kv.rf.kill()
-	// code here
+func (kv *RaftKV) runServer() {
+	DPrintf("start run kv server....")
+	for {
+		msg := <-kv.applyCh
+		DPrintf("收到 信息， 可以进行操作了")
+		op, _ := msg.Command.(Op)
+		res := Result{}
+
+		switch op.Type {
+		case "Get":
+			v, ok := kv.store[op.Key]
+			if ok {
+				res.value = v
+				res.err = OK
+			} else {
+				res.err = ErrNoKey
+			}
+		case "Put":
+			kv.store[op.Key] = op.Value
+		case "Append":
+			v, ok := kv.store[op.Key]
+			if ok {
+				kv.store[op.Key] = v + op.Value
+			} else {
+				kv.store[op.Key] = op.Value
+			}
+		}
+		DPrintf("操作结束了， debug de 的我想哭")
+		kv.response[op.Index] <- res
+
+	}
 }
 
+// StartKVServer() must retuen quickly, so it should start goroutines for any long-running work.
 func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister, maxraftstate int) *RaftKV {
 	gob.Register(Op{})
 
@@ -53,11 +139,13 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 	kv.maxraftstate = maxraftstate
 
 	// initialization code here
+	kv.store = make(map[string]string)
+	kv.response = make(map[int]chan Result)
 
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
-
 	// initialization code here
+	go kv.runServer()
 
 	return kv
 }
